d5608ebfed450e83c9d26c7b426cbf1b83ee8248
Muhammad Affan Maqsood <2021cs130@student.uet.edu.pk>
2022-03-31 17:24:46 +0500

    Add project files.

---
+++ BL/clockType.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_1.BL
+{
+    internal class clockType
+    {
+        public int hours;
+        public int minutes;
+        public int seconds;
+
+
+        public clockType()
+        {
+            hours = 12;
+            minutes = 12;
+            seconds = 12;
+        }
+
+        public clockType(int hours)
+        {
+            this.hours = hours;
+        }
+
+        public clockType(int hours, int minutes)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+        }
+
+        public clockType(int hours, int minutes, int seconds)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("{0}:{1}:{2}", hours, minutes, seconds);
+        }
+
+        public void IncrementHours()
+        {
+            hours++;
+        }
+        public void IncrementMinutes()
+        {
+            minutes++;
+        }
+        public void IncrementSeconds()
+        {
+            seconds++;
+        }
+
+        public bool IsEqual(int hours, int minutes, int seconds)
+        {
+            if(this.hours == hours && this.minutes == minutes && this.seconds == seconds)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool IsEqual(clockType InputTime)
+        {
+            if( (InputTime.hours == hours) && (InputTime.minutes == minutes) && (InputTime.seconds == seconds) )
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int ElapsedTime()
+        {
+            return (hours * 3600) + (minutes * 60) + seconds;
+        }
+        
+        public int RemainingTime()
+        {
+            int hours = 24 - this.hours;
+
+            return (hours * 3600) + (minutes * 60) + seconds;
+        }
+        public void Difference(clockType Time)
+        {
+            int hours = this.hours - Time.hours;
+            int minutes = this.minutes - Time.minutes;
+            int seconds = this.seconds - Time.seconds;
+
+            Console.WriteLine("{0}:{1}:{2}", Math.Abs(hours), Math.Abs(minutes), Math.Abs(seconds));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Challenge_1.BL;
+
+namespace Challenge_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            clockType time = new clockType(2,18,50);
+            time.Print();
+            
+            time.IncrementMinutes();
+            time.Print();
+            
+            time.IncrementHours();
+            time.Print();
+            
+            time.IncrementSeconds();
+            time.Print();
+
+            Console.WriteLine( time.IsEqual(3, 19, 51) );
+
+            Console.WriteLine(time.IsEqual(2, 18, 50));
+
+            clockType Time = new clockType(2, 29, 0);
+            Console.WriteLine(Time.IsEqual(time));
+
+            Console.WriteLine(Time.RemainingTime());
+            Console.WriteLine(Time.ElapsedTime());
+
+            Time.Difference(time);
+        }
+    }
+}
